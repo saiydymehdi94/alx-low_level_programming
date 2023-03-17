fab351bab126cc4f399fe1631db2df145a4d8e5c
3SAIYDYEL <saiydymed94@gmail.com>
2023-03-17 11:24:31 -0700

    Alx is the best

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,48 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two two-digit numbers.
+ * main - Prints numbers between 00 to 99.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int x, y;
+int i, e, g, h, op1, op2;
 
-	for (x = 0; x <= 98; x++)
+i = e = g = h = 48;
+while (h < 58)
+{
+	g = 48;
+	while (g < 58)
 	{
-		for (y = x + 1; y <= 99; y++)
+		e = 48;
+		while (e < 58)
 		{
-			putchar((x / 10) + '0');
-			putchar((x % 10) + '0');
-			putchar(' ');
-			putchar((y / 10) + '0');
-			putchar((y % 10) + '0');
-
-			if (x == 98 && x == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+			i = 48;
+			while (i < 58)
+			{
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
+				{
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				i++;
+			}
+			e++;
 		}
+		g++;
 	}
-
-	putchar('\n');
-
-	return (0);
+	h++;
+}
+putchar('\n');
+return (0);
 }
