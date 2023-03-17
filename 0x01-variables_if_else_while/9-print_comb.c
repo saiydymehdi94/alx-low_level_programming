df1238c9cc2209e2e3d7070209e0f15f470cab5e
3SAIYDYEL <saiydymed94@gmail.com>
2023-03-17 10:22:08 -0700

    hello ALX

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -prints all possible combinations of single-digit numbers.
+ *
+ *return: Always 0.
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar((x % 10) + '0');
+		if (x == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
