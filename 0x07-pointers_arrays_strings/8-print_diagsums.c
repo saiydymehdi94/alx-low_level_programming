ed6a6b1e3027e1fe748f6c4007147efffd67d61c
3SAIYDYEL <saiydymed94@gmail.com>
2023-04-03 14:00:22 -0700

    say my name

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
-* print_diagsums - sum of the two diagonals
-* @a: array
-* @size: size
-*/
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
 void print_diagsums(int *a, int size)
 {
 	int n;
