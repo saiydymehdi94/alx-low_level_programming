068c4bb025c9a8bf168c9014c2869ef3a4e45ca5
3SAIYDYEL <saiydymed94@gmail.com>
2023-04-05 14:31:17 -0700

    let s go alx

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
 /**
  * _strlen_recursion -Return the length of a staring
- * @s: The staring 
+ * @s: The staring to be measured.
+ *
  * Return: Always 0 or 1 + _strlen_recursion(s+1)
  */
 int _strlen_recursion(char *s)
