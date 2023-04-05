d9c519d47e7dbdc0178cffae1f07dd77c603c4cb
3SAIYDYEL <saiydymed94@gmail.com>
2023-04-05 16:04:24 -0700

    let s go

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,13 +13,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (y == 0)
+	else if (y == 0)
 	{
 		return (1);
 	}
-	if (y == 1)
+	else 
 	{
-		return (x);
-	}
 	return (x * _pow_recursion(x, y - 1));
+	}
 }
