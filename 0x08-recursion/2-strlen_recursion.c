57709750d716315b7b197419bbc8a52100f2bb12
3SAIYDYEL <saiydymed94@gmail.com>
2023-04-05 14:20:40 -0700

    let s go alx

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
  * _strlen_recursion -Return the length of a staring
  * @s: char
  * 
- * Return: Always NULL or 1+ _strlen_recursion(s+1)
+ * Return: Always 0 or 1 + _strlen_recursion(s+1)
  */
 int _strlen_recursion(char *s)
 {
