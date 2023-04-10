356eb4cb7d0cd4b25c35e5e4bf0e09c7cbcdf789
3SAIYDYEL <saiydymed94@gmail.com>
2023-04-10 10:59:05 -0700

    Hello alx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,13 @@
  * main -program that prints its name, followed by a new line.
  * @argc: argument counter.
  * @argv: pointer
+ *
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc > 0)
 		printf("%s\n", argv[0]);
-	return (0);
+
+		return (0);
 }
