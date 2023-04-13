e02ee0665f87ff8786923b3fde6f9f9bcd4187de
3SAIYDYEL <saiydymed94@gmail.com>
2023-04-13 11:21:29 -0700

    ALX SE

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked -allocates memory using malloc.
+ * @b: number
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *c = malloc(b);
+
+	if (c == NULL)
+	{
+		exit(98);
+	}
+	return (c);
+}
