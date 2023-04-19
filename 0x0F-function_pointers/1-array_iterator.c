52eea144104a074556a2631aad8f098ef9ea53ed
3SAIYDYEL <saiydymed94@gmail.com>
2023-04-19 14:09:27 -0700

    ALX SE

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int x;
 
+	if ( array == NULL)
+		return;
+	else if (action == 0)
+		return;
+
 	for (x = 0; x < size; x++)
 	{
 		action(array[x]);
