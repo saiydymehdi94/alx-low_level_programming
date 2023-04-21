5c29f6b9aa04ce50b03df215f50fd0c64b3376bd
3SAIYDYEL <saiydymed94@gmail.com>
2023-04-21 14:06:34 -0700

    ALX SE

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,11 +7,6 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 void array_iterator(int *array, size_t size, void (*action)(int));
 
