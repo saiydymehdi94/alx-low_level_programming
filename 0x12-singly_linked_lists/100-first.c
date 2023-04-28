f0ad31a0520630499dd9e0964cbeba2427912c8d
Saiydy Mehdi <saiydymed94@gmail.com>
2023-04-27 18:16:27 -0700

    ALX SE

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void printIntroduction(void)__attribute__((constructor));
+void first(void)__attribute__((constructor));
 /**
- * printf_fun -function
+ * first -function before the main
  * function exucuted
  */
 
-void printIntroduction(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
