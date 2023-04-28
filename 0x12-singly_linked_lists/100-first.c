f38ff497c13a3fa7e55cd8a5f07d9b359557d568
Saiydy Mehdi <saiydymed94@gmail.com>
2023-04-27 17:51:49 -0700

    ALX SE

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void printintroduction(void)__attribute__((constructor));
+/**
+ * printfbuffer _function
+ * function exucuted
+ */
+
+void printintroduction(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
