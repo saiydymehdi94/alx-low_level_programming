2489533b83ce91f0e32acd630d48f4cd36283c37
Saiydy Mehdi <saiydymed94@gmail.com>
2023-05-02 07:51:33 -0700

    ALX SE

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 -function
+ * @head: pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr, *p;
+
+	curr = *head;
+
+	while (curr != NULL)
+	{
+		p = curr;
+		curr = curr->next;
+
+		free(p);
+	}
+	*head = NULL;
+}
