756542ad95d4d379f2eabe6179433a79f920689a
Saiydy Mehdi <saiydymed94@gmail.com>
2023-05-02 11:42:22 -0700

    ALX SE

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - deletes function
+ * listint_t linked list
+ * @head: pointer
+ * @index: index of  (starting at 0)
+ *
+ * Return: 1 if the node was successfully deleted, or -1 if the deletion failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *curr = *head;
+	listint_t *p = NULL;
+	unsigned int i;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(curr);
+		return (1);
+	}
+
+	for (j = 0; j < index && curr != NULL; ++)
+	{
+		p = curr;
+		curr = curr->next;
+	}
+
+	if (curr == NULL)
+	{
+		return (-1);
+	}
+	p->next = curr->next;
+	free(curr);
+
+	return (1);
+}
