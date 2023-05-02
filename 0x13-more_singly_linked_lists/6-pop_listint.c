c52117e863115c05eed4864106e338e647f342e5
Saiydy Mehdi <saiydymed94@gmail.com>
2023-05-02 11:46:24 -0700

    ALX SE

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,7 +6,7 @@
  * pop_listint - Deletes the head node of  a listint_t linked list
  * @head: the pointer
  *
- * REturn: the head node s data (n), or O if the list is empty.
+ * Return: the head node s data (n), or O if the list is empty.
  */
 int pop_listint(listint_t **head)
 {
