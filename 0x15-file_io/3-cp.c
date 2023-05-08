9565ef4bec659f94e55fc15f30a6339221a76699
Saiydy Mehdi <saiydymed94@gmail.com>
2023-05-08 11:12:01 -0700

    ALX SE

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+/**
+ * create_buff - the allocates
+ * @file: name 
+ * Return: pointer to the newly-allocated buffer.
+ */
+char *create_buff(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buf);
+
+}
+/**
+ * cloose_file - the close file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void cloose_file(int fd)
+{
+	int i;
+
+	i = close(fd);
+
+	if (i == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * main - copies the contents s
+ * @argc: The number
+ * @argv: array
+ * Return: 0 on success.
+ * Description: if the argument specifier - exit code 97.
+ * if file_from does not exist be read - exit code 98.
+ * if file_to cannot be created  to - exit code 99.
+ * if file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buff(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+	free(buffer);
+	cloose_file(from);
+	cloose_file(to);
+	return (0);
+}
